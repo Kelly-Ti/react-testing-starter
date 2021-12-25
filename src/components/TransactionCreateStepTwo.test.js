fa19b4c6d4a9c35f3d6f9abf3da6fbe4eb7c7368
Tianying Xun <kelly@Tianyings-MBP.lan>
2021-12-25 16:06:06 +1100

    end to end

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -2,18 +2,18 @@ import { render, screen } from "@testing-library/react";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 import userEvent from "@testing-library/user-event";
 
-test("on initial render, the pay button is disabled", async () => {
-  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "6" }} />);
-
-  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
-});
-
 test("if amount and not is entered, the pay button becomes enabled", async () => {
   //arrange, render
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "6" }} />);
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
   //act user events
   userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
   userEvent.type(screen.getByPlaceholderText(/add a not/i), "dinner");
   //assertion
   expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
 });
+
+//combine units into integration, but it not about combining
+
+//few integration is better than a lot unit in a way
